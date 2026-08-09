3b5aeec875a571c621ccebfd04f3bcbd1d13947d
gallegohp <c.gallego3029@gmail.com>
2026-08-09 01:00:18 -0500

    feat: se crean consultas para el manejo directo del bloqueo

--- src/main/java/com/pulse_gym/ms_auth/repository/UserAuthRepository.java
+++ src/main/java/com/pulse_gym/ms_auth/repository/UserAuthRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.pulse_gym.lb_common.entity.auth.User;
@@ -32,8 +33,30 @@ public interface UserAuthRepository extends JpaRepository<User, Long>, JpaSpecif
      * Busca los datos de inicio de sesión de un usuario por su email.
      * 
      * @param email El email del usuario
-     * @return Un arreglo de objetos que contiene los datos de inicio de sesión del usuario, o vacío si no se encuentra
+     * @return Un arreglo de objetos que contiene los datos de inicio de sesión del
+     *         usuario, o vacío si no se encuentra
      */
     @Query("SELECT u.id, u.password, u.estado, u.failedAttempts, u.lockTime, u.rol, u.email FROM User u WHERE u.email = :email")
     Optional<Object[]> findLoginDataByEmail(@Param("email") String email);
+
+    /**
+     * Incrementa el contador de intentos fallidos de inicio de sesión de un usuario y establece el tiempo de bloqueo si se alcanza el máximo de intentos.
+     *
+     * @param userId      El ID del usuario
+     * @param maxAttempts El número máximo de intentos fallidos permitidos
+     * @return El número de filas afectadas
+     */
+    @Modifying
+    @Query("UPDATE User u SET u.failedAttempts = COALESCE(u.failedAttempts, 0) + 1, u.lockTime = CASE WHEN (COALESCE(u.failedAttempts, 0) + 1) >= :maxAttempts THEN CURRENT_TIMESTAMP ELSE u.lockTime END WHERE u.id = :userId")
+    int incrementFailedAttempts(@Param("userId") Long userId, @Param("maxAttempts") int maxAttempts);
+
+    /**
+     * Restablece el contador de intentos fallidos de inicio de sesión de un usuario y elimina el tiempo de bloqueo.
+     *
+     * @param userId El ID del usuario
+     * @return El número de filas afectadas
+     */
+    @Modifying
+    @Query("UPDATE User u SET u.failedAttempts = 0, u.lockTime = NULL WHERE u.id = :userId")
+    int resetFailedAttempts(@Param("userId") Long userId);
 }

a709a77e9edbeb082bdc2af87c720ff52e7c48fa
gallegohp <c.gallego3029@gmail.com>
2026-08-09 00:59:13 -0500

    feat: consulta para mejorar el login por emil

--- src/main/java/com/pulse_gym/ms_auth/repository/UserAuthRepository.java
+++ src/main/java/com/pulse_gym/ms_auth/repository/UserAuthRepository.java
@@ -4,10 +4,13 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import com.pulse_gym.lb_common.entity.auth.User;
 
-public interface UserAuthRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor <User> {
+import feign.Param;
+
+public interface UserAuthRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     /**
      * Busca un usuario por su email.
@@ -24,4 +27,13 @@ public interface UserAuthRepository extends JpaRepository<User, Long>, JpaSpecif
      * @return El usuario si existe, o vacío si no se encuentra
      */
     Optional<User> findByUsername(String username);
+
+    /**
+     * Busca los datos de inicio de sesión de un usuario por su email.
+     * 
+     * @param email El email del usuario
+     * @return Un arreglo de objetos que contiene los datos de inicio de sesión del usuario, o vacío si no se encuentra
+     */
+    @Query("SELECT u.id, u.password, u.estado, u.failedAttempts, u.lockTime, u.rol, u.email FROM User u WHERE u.email = :email")
+    Optional<Object[]> findLoginDataByEmail(@Param("email") String email);
 }
